7df3d1907755cdbdb017f8d8217aa2e29b132614
AdrianoCh <adrianochaves93@gmail.com>
2019-01-22 11:01:59 -0200

    add files

---
+++ br.com.simplez/src/main/java/LearningRestAssured/br/com/simplez/App.java
@@ -0,0 +1,13 @@
+package LearningRestAssured.br.com.simplez;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
