b3ca93876f5f7009c59407de9be7a32f66f2ba06
AdrianoCh <adrianochaves93@gmail.com>
2019-01-29 15:46:54 -0200

    corrected put test using hamcrest

--- br.com.simplez/src/main/java/LearningRestAssured/br/com/simplez/NewJSONTests.java
+++ br.com.simplez/src/main/java/LearningRestAssured/br/com/simplez/NewJSONTests.java
@@ -19,12 +19,32 @@ public class NewJSONTests {
         System.out.println(response.asString());
 
         response.then().body("id", hasItems("1", "2", "3", "4", "5", "6", "7", "8"));
-        response.then().body("name", hasItems("Miss Florencio Hahn", "Dameon Daniel", "Elaina Vandervort", "Willy Jacobson", "Dr. Jessy Cole", "Carlie Marvin", "Autumn Bergstrom", "Dr. Buford Stanton"));
+        response.then().body("name", hasItems("Miss Florencio Hahn", "Dameon Daniel", "Elaina Vandervort", "Willy Jacobson", "Dr. Jessy Cole", "Carlie Marvin", "Autumn Bergstrom", "Adriano Chaves"));
+    }
+
+    @Test
+    public void put_test() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"name\": \"Adriano Chaves\",\"avatar\": \"https://s3.amazonaws.com/uifaces/faces/twitter/ninjad3m0/128.jpg\"}")
+                .when()
+                .put(ROOT_URI + "/user/8");
+        System.out.println("PUT Response\n" + response.asString());
+        // tests
+        response.then().body("id", Matchers.is("8"));
+        response.then().body("name", Matchers.is("Adriano Chaves"));
+        response.then().body("avatar", Matchers.is("https://s3.amazonaws.com/uifaces/faces/twitter/ninjad3m0/128.jpg"));
     }
 
     @Test
     public void post_test(){
-        Response response = given().contentType(ContentType.JSON).accept(ContentType.JSON).body("{\"name\": \"Adriano Chaves\",\"avatar\": \"https://s3.amazonaws.com/uifaces/faces/twitter/ninjad3m0/128.jpg\"}").when().post(ROOT_URI + "/user");
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"name\": \"Adriano Chaves\",\"avatar\": \"https://s3.amazonaws.com/uifaces/faces/twitter/ninjad3m0/128.jpg\"}")
+                .when()
+                .post(ROOT_URI + "/user");
         System.out.print("Response is -->\n" + response.asString());
 
         response.then().body("name", Matchers.is("Adriano Chaves"));
